70f0fa786cb2129018172044c0341331de845bcd
davimart <81380436+davimart@users.noreply.github.com>
2023-11-26 20:34:27 -0300

    restricoes 2

---
+++ Banco de Dados/TusPeliculas/README.txt
@@ -0,0 +1,7 @@
+Projeto de banco de dados para tuspeliculas.com, um site similar ao imdb
+
+Colaboradores:
+Davi Martins 
+Mikael Ferreira 
+Victor Augusto Monteiro
+
--- Banco de Dados/TusPeliculas/create_table.sql
+++ Banco de Dados/TusPeliculas/create_table.sql
@@ -13,7 +13,7 @@ CREATE TABLE EDICAO (
   Nome_Evento VARCHAR(50) UNIQUE NOT NULL,
   Ano CUSTOM_YEAR UNIQUE NOT NULL,
   Localidade VARCHAR(50) NOT NULL,
-  FOREIGN KEY (Nome_Evento) REFERENCES EVENTO(Nome_Evento)
+  FOREIGN KEY (Nome_Evento) REFERENCES EVENTO(Nome_Evento) ON DELETE CASCADE
 );
 
 CREATE TYPE GENERO AS ENUM ('Masculino', 'Feminino', 'Não-Binário');
@@ -31,6 +31,7 @@ ID_Pessoa int PRIMARY KEY,
   Nacionalidade VARCHAR(50) NOT NULL
 );
 
+CREATE TYPE GENERO_FILME AS ENUM('Documentário', 'Não-Documentário');
 
 CREATE TABLE FILME(
 ID_Filme INT PRIMARY KEY,
@@ -42,35 +43,14 @@ ID_Filme INT PRIMARY KEY,
   Idioma_Original VARCHAR(50) NOT NULL,
   Arrec_PrimAno INT,
   Arrec_Total INT,
+  Genero_Filme GENERO_FILME NOT NULL,
  
   CONSTRAINT Estreia_Antes_Da_Prod CHECK(EXTRACT(YEAR FROM Data_Estreia) >= Ano_Prod),
   CONSTRAINT Arrecadacao CHECK(Arrec_Total >= Arrec_PrimAno)
 );
 
-CREATE TYPE CATEGORIA AS ENUM('Melhor Ator Principal', 'Melhor Ator Coadjuvante','Melhor Atriz Principal', 'Melhor Atriz Coadjuvante', 'Melhor Diretor');
 
-CREATE TABLE PREMIO (
-  ID_Edicao INT NOT NULL,
-  Nome VARCHAR(50) NOT NULL,
-  Tipo CATEGORIA NOT NULL,
-  ID_Pessoa INT NOT NULL,
-  ID_Filme INT NOT NULL,
-  Vencedor BOOL DEFAULT FALSE,
-  FOREIGN KEY (ID_Edicao) REFERENCES EDICAO(ID_Edicao),
-  PRIMARY KEY (ID_Edicao, Tipo),
-  FOREIGN KEY (ID_Pessoa) REFERENCES PESSOA(ID_Pessoa),
-  FOREIGN KEY (ID_Filme) REFERENCES FILME(ID_Filme)
-);
-
-CREATE TABLE MELHOR_FILME(
-ID_Edicao INT NOT NULL,
-  ID_Filme INT NOT NULL,
-  Vencedor BOOL DEFAULT FALSE,
-  FOREIGN KEY (ID_Edicao) REFERENCES EDICAO(ID_Edicao),
-  FOREIGN KEY (ID_Filme) REFERENCES FILME(ID_Filme),
-  PRIMARY KEY (ID_Edicao, ID_Filme)
-);
-
+/*
 CREATE TABLE DOCUMENTARIOS(
 ID_Filme INT NOT NULL,
   FOREIGN KEY (ID_Filme) REFERENCES FILME(ID_Filme),
@@ -82,28 +62,46 @@ ID_Filme INT NOT NULL,
   FOREIGN KEY (ID_Filme) REFERENCES FILME(ID_Filme),
   PRIMARY KEY (ID_Filme)
 );
+*/
 
--- Aqui, foi necessário criar as constraints depois da criação das tabelas
--- pois quando a tabela DOCUMENTARIOS é criada a tabela OUTROS_FILMES aindaDOCUMENTARIOS
--- não existe, e portanto não pode ser referenciada
+CREATE TYPE CATEGORIA AS ENUM('Melhor Ator Principal', 'Melhor Ator Coadjuvante','Melhor Atriz Principal', 'Melhor Atriz Coadjuvante', 'Melhor Diretor');
 
--- Alter table DOCUMENTARIOS add CONSTRAINT DOC_NAO_OUTROS check(not exists (Select 1 from OUTROS_FILMES where DOCUMENTARIOS.ID_Filme = OUTROS_FILMES.ID_Filme));
--- Alter table OUTROS_FILMES Add CONSTRAINT OUTROS_NAO_DOC check(ID_Filme not in (Select ID_Filme from DOCUMENTARIOS));
+CREATE TABLE PREMIO (
+  ID_Edicao INT NOT NULL,
+  Nome VARCHAR(50) NOT NULL,
+  Tipo CATEGORIA NOT NULL,
+  ID_Pessoa INT NOT NULL,
+  ID_Filme INT NOT NULL,
+  Vencedor BOOL DEFAULT FALSE,
+  FOREIGN KEY (ID_Edicao) REFERENCES EDICAO(ID_Edicao) ON DELETE CASCADE,
+  PRIMARY KEY (ID_Edicao, Tipo),
+  FOREIGN KEY (ID_Pessoa) REFERENCES PESSOA(ID_Pessoa) ON DELETE CASCADE,
+  FOREIGN KEY (ID_Filme) REFERENCES FILME(ID_Filme) ON DELETE CASCADE
+);
+
+CREATE TABLE MELHOR_FILME(
+ID_Edicao INT NOT NULL,
+  ID_Filme INT NOT NULL,
+  Vencedor BOOL DEFAULT FALSE,
+  FOREIGN KEY (ID_Edicao) REFERENCES EDICAO(ID_Edicao) ON DELETE CASCADE,
+  FOREIGN KEY (ID_Filme) REFERENCES FILME(ID_Filme) ON DELETE CASCADE,
+  PRIMARY KEY (ID_Edicao, ID_Filme)
+);
 
 CREATE TABLE DIRETOR(
 ID_Pessoa INT NOT NULL,
   ID_Filme INT NOT NULL,
   E_Principal BOOL DEFAULT FALSE,
-  FOREIGN key (ID_Pessoa) REFERENCES PESSOA(ID_Pessoa),
-  FOREIGN key (ID_Filme) REFERENCES FILME(ID_Filme),
+  FOREIGN key (ID_Pessoa) REFERENCES PESSOA(ID_Pessoa) ON DELETE CASCADE,
+  FOREIGN key (ID_Filme) REFERENCES FILME(ID_Filme) ON DELETE CASCADE,
   PRIMARY KEY (ID_Pessoa, ID_Filme)
 );
 
 CREATE TABLE PRODUTOR(
 ID_Pessoa INT NOT NULL,
   ID_Filme INT NOT NULL,
-  FOREIGN key (ID_Pessoa) REFERENCES PESSOA(ID_Pessoa),
-  FOREIGN key (ID_Filme) REFERENCES FILME(ID_Filme),
+  FOREIGN key (ID_Pessoa) REFERENCES PESSOA(ID_Pessoa) ON DELETE CASCADE,
+  FOREIGN key (ID_Filme) REFERENCES FILME(ID_Filme) ON DELETE CASCADE,
   PRIMARY KEY (ID_Pessoa, ID_Filme)
 );
 
@@ -112,24 +110,24 @@ ID_Pessoa INT NOT NULL,
   ID_Filme INT NOT NULL,
   Principal BOOL DEFAULT FALSE,
   Personagem varchar(50) NOT NULL,
-  FOREIGN key (ID_Pessoa) REFERENCES PESSOA(ID_Pessoa),
-  FOREIGN key (ID_Filme) REFERENCES FILME(ID_Filme),
+  FOREIGN key (ID_Pessoa) REFERENCES PESSOA(ID_Pessoa) ON DELETE CASCADE,
+  FOREIGN key (ID_Filme) REFERENCES FILME(ID_Filme) ON DELETE CASCADE,
   PRIMARY KEY (ID_Pessoa, ID_Filme)
 );
 
 CREATE TABLE ROTEIRISTA(
 ID_Pessoa INT NOT NULL,
   ID_Filme INT NOT NULL,
-  FOREIGN key (ID_Pessoa) REFERENCES PESSOA(ID_Pessoa),
-  FOREIGN key (ID_Filme) REFERENCES FILME(ID_Filme),
+  FOREIGN key (ID_Pessoa) REFERENCES PESSOA(ID_Pessoa) ON DELETE CASCADE,
+  FOREIGN key (ID_Filme) REFERENCES FILME(ID_Filme) ON DELETE CASCADE,
   PRIMARY KEY (ID_Pessoa, ID_Filme)
 );
 
 CREATE TABLE JURI(
 ID_Edicao INT NOT NULL,
   ID_Pessoa INT NOT NULL,
-  FOREIGN KEY (ID_Edicao) REFERENCES EDICAO(ID_Edicao),
-  FOREIGN KEY (ID_Pessoa) REFERENCES PESSOA(ID_Pessoa),
+  FOREIGN KEY (ID_Edicao) REFERENCES EDICAO(ID_Edicao) ON DELETE CASCADE,
+  FOREIGN KEY (ID_Pessoa) REFERENCES PESSOA(ID_Pessoa) ON DELETE CASCADE,
   PRIMARY KEY (ID_Edicao, ID_Pessoa)
 );
 
